fe72fd79a905daa1c76bb3184d6e1bc8c1fc13c1
Mridul-VK <72720710+Mridul-VK@users.noreply.github.com>
2023-04-03 16:16:08 +0000

    Updated package structure

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-import getFiles from "./main";
+import * as fs from "fs";
 
-module.exports = getFiles;
+export default class Fetcher {
+  getFiles(dir: string, ext: string): string[] {
+    let commandFiles: string[] = [];
+
+    let contents: fs.Dirent[] = fs.readdirSync(dir, {
+      withFileTypes: true,
+    });
+
+    for (const content of contents) {
+      if (content.isDirectory()) {
+        commandFiles = [
+          ...commandFiles,
+          ...this.getFiles(`${dir}/${content.name}`, ext),
+        ];
+      } else if (content.name.endsWith(ext)) {
+        commandFiles.push(`${dir}/${content.name}`);
+      }
+    }
+
+    return commandFiles;
+  }
+}
